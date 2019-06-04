4d869c08a68366de904487e2e56cd3f3df854b67
YamamotoMao35 <nokiroram@gmail.com>
2019-06-04 15:02:42 +0900

    Update message.rb
    Update message.rb
    
    テスト用にmessage.rbの条件を変更した。

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ApplicationRecord
   validates :title, presence: true, length: {maximum: 10}
-  validates :body, presence: true, length: {maximum: 100}
+  validates_uniqueness_of :title
+  validates :body, presence: true, length: {minimum: 20}
 
   def self.message_list
     return Message.all.order("created_at DESC")
