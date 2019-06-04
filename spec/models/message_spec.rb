3f2450f2639547ff48814f1accddb5f7dc3a647b
YamamotoMao35 <nokiroram@gmail.com>
2019-06-04 13:29:28 +0900

    Update message_spec.rb
    Update message_spec.rb
    
    message_specを導入して書き直した。

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Message, type: :model do
   describe '#create' do
     it "is invalid without a title" do
-      message = Message.new(title: "", body: "そうじゃそうじゃ")
+      message = build(:message, title: "")
       message.valid?
       expect(message.errors[:title]).to include("can't be blank")
     end
