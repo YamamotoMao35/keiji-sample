8fe6f908b3f79e2936ae9dbacbb47f4f3983768a
YamamotoMao35 <nokiroram@gmail.com>
2019-06-04 15:04:06 +0900

    Update message_spec.rb
    Update message_spec.rb
    
    テスト条件を作成し、テストできた。

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,10 +2,58 @@ require 'rails_helper'
 
 RSpec.describe Message, type: :model do
   describe '#create' do
+    # titleとbodyがあれば作成できること
+    it "is vaild with a title, body" do
+      message = build(:message)
+      expect(message).to be_valid
+    end
+
+    # titleが空では作成できないこと
     it "is invalid without a title" do
-      message = build(:message, title: "")
+      message = build(:message, title: nil)
       message.valid?
       expect(message.errors[:title]).to include("can't be blank")
+    end
+
+    # bodyが空では作成できないこと
+    it "is invalid without a body" do
+      message = build(:message, body: nil)
+      message.valid?
+      expect(message.errors[:body]).to include("can't be blank")
+    end
+
+    # titleが10文字以上であれば作成できないこと
+    it "is valid with a title that has more than 10 characters" do
+      message = build(:message, title: "aaaaaaaaaaa")
+      message.valid?
+      expect(message.errors[:title][0]).to include("is too long")
+    end
+
+    # titleが10文字以下では作成できること
+    it "is valid with a title that has less than 10 characters" do
+      message = build(:message, title: "aaaaaaaaaa")
+      expect(message).to be_valid
+    end
+
+    # 重複したtitleが存在する場合作成できないこと
+    it "is invalid with a duplicate title" do
+      message = create(:message)
+      another_message = build(:message, title: message.title)
+      another_message.valid?
+      expect(another_message.errors[:title]).to include("has already been taken")
+    end
+
+    # bodyが20文字以下であれば作成できないこと
+    it "is valid with a body that has less than 20 characters" do
+      message = build(:message, body: "aaaaaaaaaa")
+      message.valid?
+      expect(message.errors[:body][0]).to include("is too short")
+    end
+
+    # bodyが20文字以上であれば作成できること
+    it "is valid with a body that has more than 20 characters" do
+      message = build(:message, body: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+      expect(message).to be_valid
     end
   end
 end
