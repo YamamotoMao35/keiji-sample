0ec6cee8e0c1e82a7c808ee1bc5fad54ed945cf1
YamamotoMao35 <nokiroram@gmail.com>
2019-06-04 12:17:36 +0900

    Update message.rb
    Update message.rb
    
    メッセージモデルを編集した。

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,8 @@
 class Message < ApplicationRecord
+  validates :title, presence: true, length: {maximum: 10}
+  validates :body, presence: true, length: {maximum: 100}
+
+  def self.message_list
+    return Message.all.order("created_at DESC")
+  end
 end
